31ff6ef382fc2e8de99d0676c469cf419947ef65
Breno Leite <brenoleitepereira@gmail.com>
2022-07-19 17:11:51 -0300

    penultimo commit

--- js/database.js
+++ js/database.js
@@ -6,7 +6,7 @@ const data = [
     nome: "Relâmpago Marquinhos",
     categoria: "Corrida",
     description: "O Relâmpago Marquinhos é o personagem principal, ele é um personagem de corrida, o mais rápido de todos",
-    valor: 99.90,
+    valor: 99.98,
   },
   {
     id: 2,
@@ -14,7 +14,7 @@ const data = [
     nome: "Cruz Ramirez",
     categoria: "Corrida",
     description: "A Cruz Ramirez é uma personagem secundária. Importante? Talvez! No fundo acho que nem ela sabe se é importante.",
-    valor: 29.90,
+    valor: 29.98,
   },
   {
     id: 3,
@@ -22,7 +22,7 @@ const data = [
     nome: "Filmore Kombi",
     categoria: "Outros",
     description: "O Filmore é uma kombi Wolksvagen, ele é o agente da natureza, amante dos óleos naturais.",
-    valor: 19.90
+    valor: 19.99
   },
   {
     id: 4,
@@ -30,7 +30,7 @@ const data = [
     nome: "Doc Hudson",
     categoria: "Passeio",
     description: "O Hudson é o melhor professor que se pode ter, em Carros ele é um carro legal, mas na vida real ele foi quem me ensinou a fazer isso aqui!",
-    valor: 999.90
+    valor: 999.99
   },
   {
     id: 5,
@@ -38,54 +38,63 @@ const data = [
     nome: "Luigi",
     categoria: "Passeio",
     description: "O Luigi é um italiano rabugento, ele é mais conhecido por ser o chefe do grande Guido.",
-    valor: 19.90
+    valor: 19.99
   },
   {
     id: 6,
+    img: "img/guido.png",
+    nome: "Guido",
+    categoria: "Outros",
+    description: "O Guido! O grande pequenino Guido é o mais engraçado de todos, ele é o mestre da mecânica, sabe muito!",
+    valor: 299.99
+  },
+  {
+    id: 7,
     img: "img/mack.png",
     nome: "Mack",
     categoria: "Outros",
     description: "O Mack é o caminhão que deu origem a tudo! Ele dormiu no ponto e passou uma lição: Se dormir, não dirija!!",
-    valor: 39.90
+    valor: 39.99
   },
   {
-    id: 7,
+    id: 8,
     img: "img/sally.png",
     nome: "Sally",
     categoria: "Passeio",
     description: "A Sally é uma personagem legal, porém sem muito destaque nas pistas! Ela é ideal se você procura um carro belo.",
-    valor: 39.90
+    valor: 39.99
   },
   {
-    id: 8,
+    id: 9,
     img: "img/sargento.png",
     nome: "Sargento",
     categoria: "Outros",
     description: "O Sargento é um personagem rabugento, esse com certeza é o que as crianças não gostam, mas se você quiser compre!",
-    valor: 9.90
+    valor: 9.98
   },
   {
-    id: 9,
+    id: 10,
     img: "img/towmatter.png",
     nome: "Towmatter",
     categoria: "Outros",
     description: "O Towmatter é o melhor amigo do Relâmpago Marquinhos, ele é um dos melhores, sempre muito engraçado e carismático.",
-    valor: 199.90
+    valor: 199.98
   },
   {
-    id: 10,
+    id: 11,
     img: "img/trator.png",
     nome: "Trator",
     categoria: "Corrida",
     description: "O Trator é o Trator.",
-    valor: 149.90
+    valor: 149.99
   },
   {
-    id: 11,
+    id: 12,
     img: "img/xerife_dock.png",
     nome: "Dock",
     categoria: "Corrida",
     description: "O Dock é o Xerife, ele manda em tudo e corre muito atrás dos fora da lei!.",
-    valor: 119.90
+    valor: 119.98
   }
 ];
+
--- js/index.js
+++ js/index.js
@@ -1,80 +1,271 @@
 
 let secaoProdutos = document.querySelector(".products-content")
 let ulLista = document.querySelector(".listaProdutos")
+let secaoCarrinho = document.querySelector(".carrinho ul")
+
 
 secaoProdutos.classList.add("products-content")
 ulLista.classList.add("listaProdutos")
 
 
-function listarProdutos (produtos) {
+function listarProdutos(produtos, secao) {
 
-    for (let i = 0; i < produtos.length; i++) {
-        let produto = produtos[i]
-        
-        let criarCard = cardProduto(produto)
+  secao.innerHTML = ""
 
-        ulLista.appendChild(criarCard)
+  for (let i = 0; i < produtos.length; i++) {
+    let produto = produtos[i]
+
+    let criarCard = cardProduto(produto)
+
+
+    secao.appendChild(criarCard)
+  }
+
+}
+listarProdutos(data, ulLista)
+
+
+
+
+function cardProduto(produto) {
+  let nome = produto.nome
+  let img = produto.img
+  let categoria = produto.categoria
+  let descricao = produto.description
+  let valor = produto.valor
+  let id = produto.id
+
+  let tagLi = document.createElement("li")
+  let tagDiv = document.createElement("div")
+  let tagImg = document.createElement("img")
+  let tagDepartamento = document.createElement("h4")
+  let tagNome = document.createElement("h2")
+  let tagDescricao = document.createElement("p")
+  let tagPreco = document.createElement("h3")
+  let tagX = document.createElement("button")
+  let tagBtn = document.createElement("button")
+
+  if (id != undefined) {
+    tagBtn.id = id
+    tagX.id = id
+  }
+
+  tagLi.classList.add("card")
+  tagDiv.classList.add("backImg")
+  tagDepartamento.classList.add("departamento")
+  tagDescricao.classList.add("descricao")
+  tagNome.classList.add("tagNome")
+  tagDepartamento.classList.add("tagDepartamento")
+  tagPreco.classList.add("tagPreco")
+  tagBtn.classList.add("tagBtn")
+  tagX.classList.add("tagRemocao")
+
+
+
+  tagImg.src = img
+  tagImg.alt = nome
+  tagDepartamento.innerHTML = categoria
+  tagNome.innerHTML = nome
+  tagDescricao.innerHTML = descricao
+  tagPreco.innerHTML = `R$${valor}`
+  tagX.innerHTML = "Remover Produto"
+  tagBtn.innerHTML = "Adicionar ao Carrinho"
+
+  tagLi.appendChild(tagDiv)
+  tagDiv.appendChild(tagImg)
+  tagLi.append(tagDepartamento, tagNome, tagDescricao, tagPreco, tagX, tagBtn)
+
+
+  return tagLi
+}
+
+
+let inputBusca = document.querySelector(".inputBusca")
+let inputBtn = document.querySelector(".inputBtn")
+
+inputBusca.addEventListener("keyup", function () {
+
+  let valorPesquisa = inputBusca.value
+  let campoBusca = busca(valorPesquisa)
+  listarProdutos(campoBusca, secaoProdutos)
+})
+
+
+function busca(valorPesquisa) {
+  let resultBusca = []
+
+
+
+  for (let i = 0; i < data.length; i++) {
+    let pesquisa = valorPesquisa.toLowerCase()
+    let nomeProduto = data[i].nome.toLowerCase()
+    let departamento = data[i].categoria.toLowerCase()
+
+    if (nomeProduto.includes(pesquisa) || departamento.includes(pesquisa)) {
+      resultBusca.push(data[i])
+
     }
-    
+
+  }
+  if (resultBusca.length == 0 || resultBusca == "") {
+    resultBusca = data
+  }
+  return resultBusca
 }
-listarProdutos(data)
 
 
-function cardProduto (produto) {
-
-    let nome = produto.nome
-    let img = produto.img
-    let categoria = produto.categoria
-    let descricao = produto.description
-    let valor = produto.valor
-    
-
-    let tagLi = document.createElement("li")
-    let tagDiv = document.createElement("div")
-    let tagImg = document.createElement("img")
-    let tagDepartamento = document.createElement("h4")
-    let tagNome = document.createElement("h2")
-    let tagDescricao = document.createElement("p")
-    let tagPreco = document.createElement ("h3")
-    let tagBtn = document.createElement("button")
-
-    tagLi.classList.add("card")
-    tagDiv.classList.add("backImg")
-    tagDepartamento.classList.add("departamento")
-    tagDescricao.classList.add("descricao")
-    tagNome.classList.add("tagNome")
-    tagDepartamento.classList.add("tagDepartamento")
-    tagPreco.classList.add("tagPreco")
-    tagBtn.classList.add("tagBtn")
-
-
-    tagImg.src = img
-    tagImg.alt = nome
-    tagDepartamento.innerHTML = categoria
-    tagNome.innerHTML  = nome
-    tagDescricao.innerHTML  = descricao
-    tagPreco.innerHTML = `R$${valor}`
-    tagBtn.innerHTML = "Adicionar ao Carrinho"
-
-    tagLi.appendChild(tagDiv)
-    tagDiv.appendChild(tagImg)
-    tagLi.append(tagDepartamento, tagNome, tagDescricao, tagPreco, tagBtn)
-    
-
-     return tagLi
-}
 
 let btnDark = document.querySelector(".dark")
 let body = document.body
 btnDark.addEventListener("click", function () {
 
-    const classResult = body.classList.toggle("button-black")
-    if(classResult){
-        btnDark.textContent = "LIGHT MODE"
-      }else{
-        btnDark.textContent = "DARK MODE"
-      }
-       
-    })
+  const classResult = body.classList.toggle("button-black")
+  if (classResult) {
+    btnDark.textContent = "LIGHT MODE"
+  } else {
+    btnDark.textContent = "DARK MODE"
+  }
 
-console.log()
+})
+
+
+
+
+
+
+
+
+
+
+secaoProdutos.addEventListener("click", adicionarProduto)
+
+let carrinhoItens = []
+
+
+function listarCarrinho (produtos) {
+  
+
+  secaoCarrinho.innerHTML = ""
+
+  for (let i = 0; i < produtos.length; i++) {
+    let produto = produtos[i]
+
+    let carrinhoVitrine = criarCardCarrinho(produto, i)
+    secaoCarrinho.appendChild(carrinhoVitrine)
+  }
+}
+
+function criarCardCarrinho(produto, index) {
+  let nome = produto.nome
+  let img = produto.img
+  let valor = produto.valor
+  let id = produto.id
+  
+
+  let tagLi = document.createElement("li")
+  let tagNome = document.createElement("h2")
+  let tagRemocao = document.createElement("button")
+  //pode criar o event aqui
+  let tagImg = document.createElement("img")
+  let tagValor = document.createElement("h3")
+
+  tagLi.classList.add("cardCarrinho")
+  tagNome.classList.add("tagNome")
+  tagValor.classList.add("tagPreco")
+  tagRemocao.classList.add("tagRemocao")
+
+  tagRemocao.index = index
+
+  if (id != undefined) {
+    tagRemocao.id = id
+  }
+
+
+  tagNome = nome
+  tagImg.src = img
+  tagImg.alt = nome
+  tagRemocao.innerHTML = "Remover Produto"
+  tagValor = `R$${valor}`
+
+  tagLi.append(tagImg, tagNome, tagRemocao, tagValor)
+  secaoCarrinho.appendChild(tagLi)
+
+  return tagLi
+
+}
+
+function adicionarProduto(event) {
+  let btnComprar = event.target
+
+  if (btnComprar.tagName == "BUTTON") {
+    let idProduto = btnComprar.id
+
+    let produtos = data.find(function (produtos) {
+      if (produtos.id == idProduto) {
+        return produtos
+      }
+    })
+    adicionarCarrinho(produtos)
+    
+  }
+}
+
+function adicionarCarrinho(produtos) {
+
+  if (produtos !== undefined) {
+    carrinhoItens.push(produtos)
+    listarCarrinho(carrinhoItens)
+    carrinhoInfo(carrinhoItens)
+  }
+
+}
+
+function removerItem(event) {
+
+
+
+  let btnRemover = event.target
+
+  if (btnRemover.tagName == "BUTTON") {
+
+    let index = btnRemover.index
+    console.log(btnRemover)
+    secaoCarrinho.innerHTML = ""
+
+    carrinhoItens.splice(index, 1)
+    listarCarrinho(carrinhoItens)
+    carrinhoInfo(carrinhoItens)
+  }
+
+}
+secaoCarrinho.addEventListener("click", removerItem)
+
+function carrinhoInfo(arr) {
+  let carrinho_info = document.querySelector(".carrinho_info")
+  let qtdProdutos = document.querySelector(".qtdProdutos")
+  let precoTotal = document.querySelector(".precoTotal")
+
+
+  let length = arr.length
+  let preco = somar(carrinhoItens)
+
+  qtdProdutos.innerHTML = `<strong>QTD Produtos:</strong> ${length}`
+  precoTotal.innerHTML = `<strong>Preço Total:</strong> R$${preco}`
+
+}
+
+
+
+function somar(arr) {
+  let valorTotal = 0
+  for (let i = 0; i < arr.length; i++) {
+    valorTotal += arr[i].valor
+  }
+
+
+  return valorTotal
+}
+
+
+
+
